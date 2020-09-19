7fe69d9334e5fdd9b05dfee4db60c5e728c00c34
Fredemus <frederik.halkjaer1@gmail.com>
2020-09-19 23:53:20 +0200

    exercise 4

--- ECS.Legacy/NewECS/ECS.cs
+++ ECS.Legacy/NewECS/ECS.cs
@@ -6,12 +6,15 @@
         private int _thresholdVindue;
         private readonly TempSensor _tempSensor;
         private readonly Heater _heater;
+        private readonly Vindue _vindue;
 
-        public ECS(int thr)
+        public ECS(int thr, int thr2)
         {
+            SetThresholdVindue(thr2);
             SetThreshold(thr);
             _tempSensor = new TempSensor();
             _heater = new Heater();
+            _vindue = new Vindue();
         }
 
         public void Regulate()
@@ -20,18 +23,42 @@
             if (t < _threshold)
                 _heater.TurnOn();
             else
+            {
                 _heater.TurnOff();
+                // Siden Heater ikke har en state variabel, kan vindue kun åbnes, hvis heater er blevet slukket
+                if (t > _thresholdVindue)
+                    _vindue.Open();
+            }
+            // men Vindue kan lukkes uanset om heater er tændt eller ej.
+            // Kunne ikke se en smartere måde uden at tilføje en variabel til hear
+            if (t < _thresholdVindue)
+                _vindue.Close();
 
         }
 
         public void SetThreshold(int thr)
         {
-            _threshold = thr;
+            if (thr < _thresholdVindue)
+                _threshold = thr;
+            else
+                System.Console.WriteLine("Heater threshold must be lower than Window threshold");
         }
 
         public int GetThreshold()
         {
             return _threshold;
+        }
+        public void SetThresholdVindue(int thr)
+        {
+            if (thr > _threshold)
+                _thresholdVindue = thr;
+            else
+                System.Console.WriteLine("Window threshold must be higher than heater threshold");
+        }
+
+        public int GetThresholdVindue()
+        {
+            return _thresholdVindue;
         }
 
         public int GetCurTemp()
--- ECS.Legacy/NewECS/Program.cs
+++ ECS.Legacy/NewECS/Program.cs
@@ -2,11 +2,12 @@
 
 namespace NewECS
 {
+    /// Det her giver problemer, da det giver mere end et entry point
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello World!");
+        //}
     }
 }
